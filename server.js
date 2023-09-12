2abe1cbaa8ad1588fb36fbe2f60414957b0c6941
yenglikSalamatova <yengliksalamatova@gmail.com>
2023-09-12 20:27:28 +0600

    add cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const passport = require("passport");
 const jwtStrategy = require("./server/passport/jwtStrategy");
 const sequelize = require("./server/config/database");
+const cors = require("cors");
 const app = express();
 
 const authRoutes = require("./server/routes/authRoutes");
@@ -14,6 +15,7 @@ const subscriptionRoutes = require("./server/routes/subscriptionRoutes");
 const likeRoutes = require("./server/routes/likeRoutes");
 
 // Parsers
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "public"));
