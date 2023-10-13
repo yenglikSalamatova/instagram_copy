1429a127bd16568239ca9010216286c44c56abf8
yenglikSalamatova <yengliksalamatova@gmail.com>
2023-10-13 09:46:44 +0600

    go back

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-const https = require("https");
-const fs = require("fs");
 const express = require("express");
 require("dotenv").config();
 const passport = require("passport");
@@ -33,13 +31,6 @@ app.use("/api/comments", commentRoutes);
 app.use("/api/subscriptions", subscriptionRoutes);
 app.use("/api/likes", likeRoutes);
 
-const options = {
-  key: fs.readFileSync("backend.key"),
-  cert: fs.readFileSync("backend.crt"),
-};
-
-const server = https.createServer(options, app);
-
-server.listen(3000, () => {
+app.listen(3000, () => {
   console.log("Server is listening on port 3000 with SSL certificate");
 });
