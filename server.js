bd5052cefd0ed8145745921c3d8e968864e69ec5
yenglikSalamatova <yengliksalamatova@gmail.com>
2023-11-21 05:57:58 +0600

    neon db 2

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.use("/api/comments", commentRoutes);
 app.use("/api/subscriptions", subscriptionRoutes);
 app.use("/api/likes", likeRoutes);
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000 with SSL certificate");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
